3381a3ef2d11053362e7b324916da11459713a87
Jamie (JT) O'Roke <56612513+oroke24@users.noreply.github.com>
2024-07-09 19:45:19 -0700

    Testing Firestore DB in test.js and book_now.html

--- js/test.js
+++ js/test.js
@@ -1,13 +1,30 @@
-console.log("opening test.js");
-const form = document.getElementById("book_now-form");
-const firstName = document.getElementById("firstName");
-const submit = document.getElementById("submit");
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+//unique firebase object
+const firebaseConfig = {
+    apiKey: "AIzaSyCDGvCeUX3po6mvddWprnM1HUDipEmHfyE",
+    authDomain: "wnrmc-e91c3.firebaseapp.com",
+    projectId: "wnrmc-e91c3",
+    storageBucket: "wnrmc-e91c3.appspot.com",
+    messagingSenderId: "832604211915",
+    appId: "1:832604211915:web:fc2010abad9b65a3bee94e",
+    measurementId: "G-6QVB8R39SN"
+};
+//Initialize db
+firebase.initializeApp(firebaseConfig);
+var firebase = firebase.firestore();
+
+//variable to access db collection
+const db = firestore.collection("users");
+db.doc().set({
+    name: "hi wurld"})
+
+//Get submit form
+let submit = document.getElementById("submit");
+//create Event Listener to allow form submission
 submit.addEventListener("click", (e) => {
+    alert("helloworld");
+    //prevent default behavior
     e.preventDefault();
-    db.collection('').doc().set({
-        firstName: firstName.value
-    }).then(() => {
-        book_now-form.reset();
-        con
-    })
 });
