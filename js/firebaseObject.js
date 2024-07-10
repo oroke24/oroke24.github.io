f8f0d90698aceb350543677d546b7aae8ce7fab6
Jamie (JT) O'Roke <56612513+oroke24@users.noreply.github.com>
2024-07-10 00:57:39 -0700

    updating imports

--- js/firebaseObject.js
+++ js/firebaseObject.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
+import firebase from '../node_modules/firebase/compat/app';
+import '../node_modules/firebase/compat/firestore';
 
 //unique firebase object
 const firebaseConfig = {
