5c0ea0049ecdd5ab95e6f4a59b6b94499de77981
Jamie (JT) O'Roke <56612513+oroke24@users.noreply.github.com>
2024-07-12 00:22:53 -0700

    updating employee password

--- js/employeeLock.js
+++ js/employeeLock.js
@@ -2,7 +2,7 @@ function openPageWithMasterPassword(pathString) {
     // Prompt the user for their master password
     let userEnteredPassword = prompt("Please enter admin password:");
     // Assuming you have a stored master password
-    let storedMasterPassword = "drinkcoffee";
+    let storedMasterPassword = "Drinkcoffee";
     // Check if the entered password matches the stored master password
     if (userEnteredPassword === storedMasterPassword) {
         // Proceed
