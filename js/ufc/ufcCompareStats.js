953eb16f7257220557a66c60a491b60903fd2733
Jamie (JT) O'Roke <56612513+oroke24@users.noreply.github.com>
2024-09-22 14:09:15 -0700

    Update ufcCompareStats.js

--- js/ufc/ufcCompareStats.js
+++ js/ufc/ufcCompareStats.js
@@ -5,6 +5,14 @@ function compareStats(f1, f2) {
     var winDiff;
     var wlRatioDiff;
     var ageDiff;
+    var sspm; 
+    var sspct;
+    var tdavg;
+    var tdpct;
+    var ssabs;
+    var ssdef;
+    var tddef;
+    var subavg;
     var extraDamage;
     var takeDownsLanded;
     var timesTakenDown;
@@ -14,10 +22,26 @@ function compareStats(f1, f2) {
     reachDiff = f1.reach - f2.reach;
     winDiff = f1.wins -f2.wins;
     wlRatioDiff = f1.wlRatio - f2.wlRatio;
-     
+     sspm = f1.sigStrLandPm - f2.sigStrLandPm;
+     sspct = f1.sigStrLandPct - f2.sigStrLandPct;
+     tdavg = f1.tdAvg- f2.tdAvg;
+     tdpct = f1.tdLandPct - f2.tdLandPct;
+     ssabs = f1.sigStrAbsPm - f2.sigStrAbsPm;
+     ssdef = f1.sigStrDefPct - f2.sigStrDefPct;
+     tddef = f1.tdDefPct - f2.tdDefPct;
+     subavg = f1.subAvg - f2.subAvg;
+    
     return `ageDiff = ${ageDiff}, 
             heightDiff = ${heightDiff}, 
             reachDiff = ${reachDiff}, 
             winDiff = ${winDiff}, 
-            wlRatioDiff = ${wlRatioDiff}`;
+            wlRatioDiff = ${wlRatioDiff},
+            sspmDiff = ${sspm},
+            sspctDiff = ${sspct},
+            tdavg = ${tdavg},
+            tdpct = ${tdpct}, 
+            ssabsDiff = ${ssabs},
+            ssdefDiff = ${ssdef},
+            tddefDiff = ${tddef},
+            subavgdiff = ${subavg}';
 }
