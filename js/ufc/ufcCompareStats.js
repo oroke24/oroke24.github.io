f090a3b1be43f7880720ffe86dcd45f810dcd258
Jamie (JT) O'Roke <56612513+oroke24@users.noreply.github.com>
2024-09-22 02:16:34 -0700

    Update ufcCompareStats.js

--- js/ufc/ufcCompareStats.js
+++ js/ufc/ufcCompareStats.js
@@ -1,5 +1,10 @@
 function compareStats(f1, f2) {
     var winner;
+    var heightDiff;
+    var reachDiff;
+    var winDiff;
+    var wlRatio;
+    var ageDiff;
     if(f1.wins-f2.wins > 0 ) winner = f1; 
     else winner = f2;
     
