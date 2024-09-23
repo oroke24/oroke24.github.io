643f8f4ecb46834641fc00d0b2b9f4cea1a9422b
Jamie (JT) O'Roke <56612513+oroke24@users.noreply.github.com>
2024-09-22 21:15:51 -0700

    Update ufcCompareStats.js

--- js/ufc/ufcCompareStats.js
+++ js/ufc/ufcCompareStats.js
@@ -18,25 +18,25 @@ function compareStats(f1, f2) {
     var timesTakenDown;
     
     ageDiff = f1.age - f2.age; 
-    heightDiff = (f1.heightInches - f2.heightInches).toFixed(2);
-    reachDiff = (f1.reach - f2.reach).toFixed(2);
-    winDiff = (f1.wins -f2.wins).toFixed(2);
-    wlRatioDiff = (f1.wlRatio - f2.wlRatio).toFixed(2);
-     sspm = (f1.sigStrLandPm - f2.sigStrLandPm).toFixed(2);
-     sspct = (f1.sigStrLandPct - f2.sigStrLandPct).toFixed(2);
-     tdavg = (f1.tdAvg- f2.tdAvg).toFixed(2);
-     tdpct = (f1.tdLandPct - f2.tdLandPct).toFixed(2);
-     ssabs = (f1.sigStrAbsPm - f2.sigStrAbsPm).toFixed(2);
-     ssdef = (f1.sigStrDefPct - f2.sigStrDefPct).toFixed(2);
-     tddef = (f1.tdDefPct - f2.tdDefPct).toFixed(2);;
-     subavg = (f1.subAvg - f2.subAvg).toFixed(2);
+    heightDiff = f1.heightInches - f2.heightInches;
+    reachDiff = f1.reach - f2.reach;
+    winDiff = f1.wins -f2.wins);
+    wlRatioDiff = f1.wlRatio - f2.wlRatio;
+     sspm = f1.sigStrLandPm - f2.sigStrLandPm;
+     sspct = f1.sigStrLandPct - f2.sigStrLandPct;
+     tdavg = f1.tdAvg- f2.tdAvg;
+     tdpct = f1.tdLandPct - f2.tdLandPct;
+     ssabs = f1.sigStrAbsPm - f2.sigStrAbsPm;
+     ssdef = f1.sigStrDefPct - f2.sigStrDefPct;
+     tddef = f1.tdDefPct - f2.tdDefPct;
+     subavg = f1.subAvg - f2.subAvg;
     
     
      let basicCompare = heightDiff + winDiff + sspm + sspct + tdavg + tdpct + ssabs + ssdef + tddef + subavg;
     if(basicCompare > 0) winner = f1.name;
     else if(basicCompare < 0) winner = f2.name;
     else winner = "No Advantage";
-    let absBC = Math.abs(basicCompare);
+    let absBC = Math.abs(basicCompare).toFixed(2);
     
     
     return `<br><br><br> <strong>Advantage goes to ${winner}</strong> Basic Comparison = ${absBC}<br><br>heightDiff = ${heightDiff} in.,<br>
