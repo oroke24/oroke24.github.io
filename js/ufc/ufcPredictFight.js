75bb663a4f84e9d26093d3d0134488f1d5ef8170
Jamie (JT) O'Roke <56612513+oroke24@users.noreply.github.com>
2024-09-28 15:13:37 -0700

    changed AI response word limit from 1000 to 500 words

--- js/ufc/ufcPredictFight.js
+++ js/ufc/ufcPredictFight.js
@@ -1,7 +1,7 @@
 async function predictFight(f1, f2, advantages, roundSimulation) {
-    const userMessage = `(using "###" to represent new paragraph) Estimate the outcome and list the stats according to betting odds.  Use the following information:${f1.name}, ${f2.name}. Stat Comparison: ${advantages}.  Round simulation: ${roundSimulation} (start by clearly announcing the decided winner) `;
+    const userMessage = `(using "###" to represent new paragraph) Estimate the outcome and list the stats according to betting odds.  Use the following information:${f1.name}, ${f2.name}. Stat Comparison: ${advantages}.  Round simulation: ${roundSimulation} .  Limit to around 500 words or less(start by clearly announcing the decided winner) `;
     const assistantType = "You are a UFC fight outcome predictor that recieves information about two fighters and predicts betting odds for knockout, submission, or decision, also predicting lengt of fight if not decision and significant strikes. provide the response in clean separated paragraphs";
-    let aiPrompt = `(using "###" to represent new paragraph) Predict the outcome if these two fighters fought, include how the statistics would look and include betting odds. limit to around 1000 words or less: ${f1.name} vs. ${f2.name}.(start by clearly announcing your decided winner)`;
+    let aiPrompt = `(using "###" to represent new paragraph) Predict the outcome if these two fighters fought, include how the statistics would look and include betting odds. limit to around 500 words or less: ${f1.name} vs. ${f2.name}.(start by clearly announcing your decided winner)`;
     let userMsg = await fightPrompt(userMessage, assistantType);
     let whatAiThinks = `<br><br> So all that was <strong> according to Jamie's calculations</strong>.  Heres what I think. \n`;
     whatAiThinks += await fightPrompt(aiPrompt, assistantType);
